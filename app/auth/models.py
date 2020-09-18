f0f69e66750f3f3d17a1a73c381c412e97fedd10
Greger Stolt Nilsen <gregersn@gmail.com>
2020-09-18 22:43:36 +0200

    Minor linting and config changes.

--- app/auth/models.py
+++ app/auth/models.py
@@ -6,20 +6,22 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-    profile = db.relationship('UserProfile', uselist=False, back_populates="user")
+    profile = db.relationship('UserProfile', uselist=False,
+                              back_populates="user")
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
@@ -27,11 +29,12 @@ class User(UserMixin, db.Model):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
             current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
-    
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
-        except:
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except Exception:
             return
         return User.query.get(id)
--- config.py
+++ config.py
@@ -1,14 +1,17 @@
+"""App default config."""
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config():
+    """Settings to be overridden with env variables."""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'development'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'whathappened.sqlite')
+        'sqlite:///' + os.path.join(BASEDIR, 'whathappened.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER="localhost"
-    MAIL_PORT=8025
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or "localhost"
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 8025, 10)
     ADMINS = ['your-email@example.com']
     ASSETS_DEBUG = False
     ASSETS_AUTO_BUILD = True
-
