250a2f843203c93f60f353cb46018cc803b0fc79
Greger Stolt Nilsen <gregersn@gmail.com>
2020-10-21 20:43:11 +0200

    Add build and watch command to trigger webpack.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,3 +4,28 @@ bp = Blueprint('main', __name__, template_folder='templates')
 api = Blueprint('main', __name__, template_folder='templates')
 
 from . import routes  # noqa: E402, F401 isort:skip
+
+import logging
+import click
+
+from pywebpack import WebpackProject
+
+logger = logging.getLogger(__name__)
+
+
+@bp.cli.command('build')
+def build():
+    logger.debug("Build stuff")
+    project_path = './frontend'
+    project = WebpackProject(project_path)
+
+    project.build()
+
+@bp.cli.command('watch')
+def watch():
+    logger.debug("Watch stuff")
+    project_path = './frontend'
+    project = WebpackProject(project_path)
+
+    project.run('watch')
+
