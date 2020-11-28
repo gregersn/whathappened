ab0320831911b5073a0b21e02bc13e03d0a0d42b
Greger Stolt Nilsen <gregersn@gmail.com>
2020-11-28 18:08:26 +0100

    Fix crash when logging occupation checkmarks.

--- app/character/routes.py
+++ app/character/routes.py
@@ -127,7 +127,11 @@ def update(id):
 
         for setting in update:
             character.set_attribute(setting)
-            log_message = f"set {setting['field']}{' ' + setting.get('subfield', '') if setting.get('subfield', 'None') != 'None' else '' }: {setting['value']}"
+            field = setting['field']
+            subfield = setting.get('subfield', '')
+            value = setting['value']
+            type = setting.get('type', 'value')
+            log_message = f"set {type} on {field}{' ' + subfield if subfield is not None and subfield != 'None' else '' }: {value}"
             logentry = LogEntry(character, log_message, user_id=current_user.id)
             db.session.add(logentry)
 
