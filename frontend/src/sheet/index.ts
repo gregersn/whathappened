f1ad4515d863166bea133270b110354a39f8b503
Greger Stolt Nilsen <gregersn@gmail.com>
2020-11-29 23:50:19 +0100

    Start working on a value updater for char sheet.

--- frontend/src/sheet/index.ts
+++ frontend/src/sheet/index.ts
@@ -23,7 +23,33 @@ function init_sharebutton() {
     }
 }
 
+function init_valueadjusters() {
+    const adjusters: HTMLDivElement[] = <HTMLDivElement[]>Array.from(document.getElementsByClassName('value_adjuster'));
+    adjusters.forEach(adjuster => {
+        console.log("Setting up adjuster");
+        const datafield = adjuster.getAttribute('data-field');
+        console.log("For field " + datafield);
+
+        const input = <HTMLInputElement>adjuster.getElementsByTagName('input')[0];
+        const button_add = <HTMLButtonElement>adjuster.getElementsByClassName('add')[0];
+        const button_subtract = <HTMLButtonElement>adjuster.getElementsByClassName('subtract')[0];
+
+        button_add.onclick = () => {
+            if(!input.valueAsNumber) return;
+            console.log(`Add ${input.valueAsNumber}`);
+            input.value = '';
+        }
+        
+        button_subtract.onclick = () => {
+            if(!input.valueAsNumber) return;
+            console.log(`Subtract ${input.valueAsNumber}`);
+            input.value = '';
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function(event) {
     console.log("Initiate the basic character sheet.")
     init_sharebutton();
+    init_valueadjusters();
 })
