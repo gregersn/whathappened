11ea97498fb868ce463b1eea3dba9e5516b47bc9
Greger Stolt Nilsen <gregersn@gmail.com>
2020-12-19 10:33:54 +0100

    Reorganize frontend scripts.

--- frontend/src/coc7e/index.ts
+++ frontend/src/coc7e/index.ts
@@ -1,5 +1,5 @@
-import { get_meta_tag, Listdata, make_element_editable, saveCheck, SaveFunction, Tabledata } from "../common"
-import { send_update, Datamap } from "../common"
+import { saveCheck } from "../common"
+import { send_update, init_set_portrait } from "../common"
 
 
 function init_skillchecks() {
@@ -15,31 +15,6 @@ function init_skillchecks() {
     });
 }
 
-
-let popup: HTMLDivElement = null;
-function init_popup() {
-    popup = document.createElement('div');
-    popup.style.backgroundColor = "#ff0000";
-    popup.style.width = "100px";
-    popup.style.height = "50px";
-    popup.style.position = "absolute";
-    popup.style.top = "100px";
-    popup.style.left = "100px";
-    popup.hidden = true;
-    document.body.appendChild(popup);
-}
-
-function show_popup(x: number, y: number, content: HTMLElement) {
-    popup.style.left = x + "px";
-    popup.style.top = y + "px";
-    popup.hidden = false;
-    popup.innerHTML = "";
-    popup.appendChild(content);
-}
-
-function close_popup() {
-    popup.hidden = true;
-}
 
 function show_subskillform(x: number, y: number, parent: string) {
     const formcontainer: HTMLDivElement = <HTMLDivElement>document.getElementById("subskillform");
@@ -100,166 +75,9 @@ function init_skill_edits() {
     })
 }
 
-function table_to_obj(table: HTMLTableElement): Tabledata {
-    let data_rows = [];
-
-    //const tableName = table.getAttribute('data-field');
-    const fields = Array.from(table.tHead.rows[0].cells).map((element, index) => {
-        return {'property': element.getAttribute('data-property'), 'index': index};
-    }).filter((element, index) => {
-        if(element['property']) return true;
-        return false;
-    });
-
-    const rows = Array.from(table.tBodies[0].rows);
-    for(const row of rows) {
-        const row_data = {}
-        fields.forEach(field => {
-            row_data[field['property']] = row.cells.item(field['index']).innerHTML;
-        });
-
-        data_rows.push(row_data);
-    }
-    /*data_rows.sort((a, b) => { 
-        if( a['name'] < b['name'] ) return -1;
-        if( a['name'] > b['name'] ) return 1;
-        return 0;})*/
-
-    return data_rows
-}
-
-function list_to_obj(list: HTMLUListElement): Listdata {
-    let data_rows  = []
-    const lines: HTMLLIElement[] = Array.from(list.getElementsByTagName('li'));
-    for(const line of lines) {
-        data_rows.push(line.innerHTML);
-    }
-    return data_rows;
-}
-
-const editable_list = (list: HTMLUListElement, save: (data: Listdata) => void) => {
-    console.log("Editable list...");
-    const lines: HTMLLIElement[] = Array.from(list.getElementsByTagName('li'));
-    for(const line of lines) {
-        make_element_editable(line, (data: any) => {
-            save(list_to_obj(list));
-        })
-    }
-
-    const parent = list.parentElement;
-    const button = document.createElement('button');
-    button.innerHTML = "Add item";
-    button.onclick = () => {
-        const new_item = document.createElement('li');
-        new_item.innerHTML = 'New item...';
-        list.appendChild(new_item);
-        make_element_editable(new_item, (data: any) => {
-            save(list_to_obj(list));
-        })
-    }
-    parent.appendChild(button);
-}
-
-const editable_table = (table: HTMLTableElement, save: (data: Tabledata) => void) => {
-    const make_cell_editable = (cell: HTMLTableCellElement) => {
-        make_element_editable(cell, (data: any) => {
-            save(table_to_obj(table));
-        });
-    }
-    const make_row_editable = (row: HTMLTableRowElement, fields: any[]) => {
-        const cells = Array.from(row.cells);
-        //console.log(fields);
-        cells.forEach((cell, index) => {
-            if(fields.find(field => (field['index'] === index))) {
-                make_cell_editable(cell);
-            }
-        })
-    }
-
-    const cells = Array.from(table.tHead.rows[0].cells);
-    const fields = cells.map((element, index) => {
-        return {'property': element.getAttribute('data-property'), 'index': index};
-    }).filter((element, index) => {
-        if(element['property']) return true;
-        return false;
-    });
-
-    const rows =  Array.from(table.tBodies[0].rows);
-    for(const row of rows) {
-        make_row_editable(row, fields);
-    }
-    const parent = table.parentElement;
-    const button = document.createElement('button');
-    button.innerHTML = "Add row";
-
-    button.onclick = () => {
-        const new_row = table.insertRow(-1);
-        new_row.innerHTML = "<td>-</td>".repeat(cells.length);
-        make_row_editable(new_row, fields);
-    }
-
-    parent.appendChild(button);
-}
-
-
-
-function init_editable_tables() {
-    console.log("Init editable tables");
-    const tables: HTMLTableElement[] = <HTMLTableElement[]>Array.from(document.getElementsByClassName('editableTable'));
-
-    tables.forEach(table => {
-        editable_table(table, (data: Tabledata) => {
-            const field = table.getAttribute('data-field');
-            console.log("Saving table.\n"); console.log(data)
-            send_update({field: field}, data)
-        } );
-    });
-
-}
-
-function init_editable_lists() {
-    console.log("Init editable lists");
-    const lists = <HTMLUListElement[]>Array.from(document.getElementsByClassName("editable_list"))
-
-    lists.forEach(list => {
-        editable_list(list, (data: Listdata) => {
-            const field = list.getAttribute('data-field');
-            console.log("Saving list.\n"); console.log(data);
-            send_update({field: field}, data)
-        })
-    })
-}
-
-function init_set_portrait() {
-    console.log("Init set portrait");
-    const portraitbox: HTMLElement = <HTMLElement>document.getElementsByClassName('portrait')[0];
-    if(portraitbox.classList.contains("editable")) {
-        const uploadelement: HTMLInputElement = document.createElement('input');
-        uploadelement.type = 'file';
-        uploadelement.style.display = 'none';
-        uploadelement.onchange = (e: Event) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(uploadelement.files[0]);
-            reader.onload = () => {
-                send_update({field: 'personalia.Portrait', type: 'portrait'}, reader.result);
-            }
-        }
-
-        portraitbox.appendChild(uploadelement);
-        portraitbox.onclick = (e: Event) => {
-            uploadelement.click();
-        }
-    }
-}
-
-
 document.addEventListener('DOMContentLoaded', function(event) {
     console.log("Cthulhu fhtagn!");
-
-    init_popup();
     init_skillchecks();
-    init_editable_tables();
-    init_editable_lists();
     init_skill_edits();
     init_set_portrait();
   })
--- frontend/src/sheet/index.ts
+++ frontend/src/sheet/index.ts
@@ -1,5 +1,6 @@
 import { make_element_editable, saveCheck, show_message } from "../common";
 import { send_update, Datamap, Elementdata, Tabledata } from "../common";
+import { editable_list, editable_table, Listdata } from "../common"
 
 function init_sharebutton() {
     const button = document.getElementById('sharebtn')
@@ -61,11 +62,66 @@ function init_editable_binaries() {
     });
 }
 
+function init_editable_lists() {
+    console.log("Init editable lists");
+    const lists = <HTMLUListElement[]>Array.from(document.getElementsByClassName("editable_list"))
 
+    lists.forEach(list => {
+        editable_list(list, (data: Listdata) => {
+            const field = list.getAttribute('data-field');
+            console.log("Saving list.\n"); console.log(data);
+            send_update({field: field}, data)
+        })
+    })
+}
+
+
+let popup: HTMLDivElement = null;
+function init_popup() {
+    popup = document.createElement('div');
+    popup.style.backgroundColor = "#ff0000";
+    popup.style.width = "100px";
+    popup.style.height = "50px";
+    popup.style.position = "absolute";
+    popup.style.top = "100px";
+    popup.style.left = "100px";
+    popup.hidden = true;
+    document.body.appendChild(popup);
+}
+
+function show_popup(x: number, y: number, content: HTMLElement) {
+    popup.style.left = x + "px";
+    popup.style.top = y + "px";
+    popup.hidden = false;
+    popup.innerHTML = "";
+    popup.appendChild(content);
+}
+
+function close_popup() {
+    popup.hidden = true;
+}
+
+
+function init_editable_tables() {
+    console.log("Init editable tables");
+    const tables: HTMLTableElement[] = <HTMLTableElement[]>Array.from(document.getElementsByClassName('editableTable'));
+
+    tables.forEach(table => {
+        editable_table(table, (data: Tabledata) => {
+            const field = table.getAttribute('data-field');
+            console.log("Saving table.\n"); console.log(data)
+            send_update({field: field}, data)
+        } );
+    });
+
+}
 
 document.addEventListener('DOMContentLoaded', function(event) {
     console.log("Initiate the basic character sheet.")
+    init_popup();
     init_sharebutton();
     init_editable();
+    init_editable_lists();
     init_editable_binaries();
+    init_editable_tables();
 })
