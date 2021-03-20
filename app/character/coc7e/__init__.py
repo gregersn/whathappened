935e66100f665f1e047923916ba9a895ec8e27ab
Greger Stolt Nilsen <gregersn@gmail.com>
2021-03-20 14:37:11 +0100

    Simplify importing of extra game systems.

--- app/character/coc7e/__init__.py
+++ app/character/coc7e/__init__.py
@@ -4,9 +4,11 @@ import jinja2
 import time
 from typing import Literal
 
-from ..core import register_game
+
 from .mechanics import CoCMechanics
 from .mechanics import schema_file  # noqa F401
+
+from ..core import register_game
 
 logger = logging.getLogger(__name__)
 
@@ -18,17 +20,18 @@ from .forms import CreateForm  # noqa F401
 from .routes import view  # noqa F401
 
 CHARACTER_TEMPLATE = 'character/coc7e/blank_character.json.jinja'
+CREATE_TEMPLATE = 'character/coc7e/create.html.jinja'
 
 
-def new_character(title, gametype: GameType):
+def new_character(title: str, gametype: GameType, **kwargs):
     templateloader = jinja2 \
-                     .FileSystemLoader(searchpath="./app/character/templates/")
+        .FileSystemLoader(searchpath="./app/character/templates/")
     templateenv = jinja2.Environment(loader=templateloader)
     template = templateenv.get_template(CHARACTER_TEMPLATE)
     gtype = gametype
     return json.loads(template.render(title=title,
                                       timestamp=time.time(),
-                                      type=gtype))
+                                      gametype=gtype))
 
 
 register_game('coc7e', 'Call of Cthulhu TM', CoCMechanics)
