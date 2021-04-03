94c95a57cf29d675dceff6a7656cda6046edf708
Greger Stolt Nilsen <gregersn@gmail.com>
2021-04-03 20:18:07 +0200

    Stuffs

--- app/character/swd6/__init__.py
+++ app/character/swd6/__init__.py
@@ -1,11 +1,15 @@
-import yaml
+import os
 import logging
 import pathlib
+
+from app.character.schema import load_schema, build_from_schema
 
 from ..core import register_game
 from .mechanics import SWD6Mechanics
 
 logger = logging.getLogger(__name__)
+
+schema_file = os.path.join(os.path.dirname(__file__), '../schema/swd6.yaml')
 
 BLANK_CHARACTER = pathlib.Path(
     __file__).parent.joinpath('blank_character.yaml')
@@ -14,11 +18,12 @@ CHARACTER_SHEET_TEMPLATE = 'character/swd6/sheet.html.jinja'
 
 
 def new_character(title: str, **kwargs):
-    with open(BLANK_CHARACTER, 'r') as f:
-        data = yaml.safe_load(f)
-        data['meta']['title'] = title
+    schema = load_schema(schema_file)
 
-        return data
+    nc = build_from_schema(schema)
+    nc['title'] = title
+
+    return nc
 
 
 register_game('swd6', 'Star Wars WEG', SWD6Mechanics)
---
+++ tests/swd6/test_swd6.py
@@ -0,0 +1,27 @@
+"""Test functions related to Star Wars WEG D6."""
+import os
+import pytest
+
+from jsonschema import validate
+
+from app.character.schema import load_schema
+from app.character.swd6 import schema_file
+from app.character.swd6 import new_character
+from app.character.models import Character
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+@pytest.fixture(name="newly_created_character")
+def fixture_test_character() -> Character:
+    nc = new_character("Test Character")
+    c = Character(title="Test Character",
+                  body=nc)
+
+    return c
+
+
+def test_validate():
+    nc = new_character("Test Character")
+    schema = load_schema(schema_file)
+    validate(nc, schema=schema)
