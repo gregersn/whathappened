b2a89f2b735b818b4ae2f771468ff56ea0775845
Greger Stolt Nilsen <gregersn@gmail.com>
2021-07-30 23:48:12 +0200

    Move config file inside main package.

--- whathappened/config.py
+++ whathappened/config.py
@@ -10,8 +10,6 @@ load_dotenv(os.path.join(BASEDIR, '.env'))
 class Config():
     """Settings to be overridden with env variables."""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'development'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(BASEDIR, 'instance', 'whathappened.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or "localhost"
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 8025)
