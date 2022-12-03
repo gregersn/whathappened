184342a7b3c70fa101567698667303d3023419da
Greger Stolt Nilsen <gregersn@gmail.com>
2022-12-03 01:04:59 +0100

    Fix neglected setting files.

--- whathappened/config.py
+++ whathappened/config.py
@@ -9,14 +9,13 @@ class Settings(BaseSettings):
     """Settings to be overridden with env variables."""
     ADMINS: List[str] = ['your-email@example.com']
     ASSETS_DEBUG: bool = False
-    FLASK_ENV: str = 'development'
+    FLASK_DEBUG: bool = True
     MAIL_PORT: int = 8025
     MAIL_SERVER: str = "localhost"
     MAX_CONTENT_LENGTH: int = 1024 * 1024  # Max upload size
     SECRET_KEY: str = "development"
     SQLALCHEMY_DATABASE_URI: Optional[str] = os.environ.get(
-        'DATABASE_URL'
-    ) or f"sqlite:///{Path(__file__).parent.parent / 'instance' / 'whathappened.sqlite'}"
+        'DATABASE_URL') or f"sqlite:///{Path(__file__).parent.parent / 'instance' / 'whathappened.sqlite'}"
 
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
     UPLOAD_EXTENSIONS: List[str] = ['.jpg', '.png', '.jpeg', '.gif']
