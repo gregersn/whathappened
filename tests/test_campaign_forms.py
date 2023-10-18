bb8e95a999d0625fa02bff50e730d25ea4bfdf02
Greger Stolt Nilsen <gregersn@gmail.com>
2023-10-18 09:10:25 +0200

    Add basic form test.

---
+++ tests/test_campaign_forms.py
@@ -0,0 +1,23 @@
+import pytest
+from markupsafe import Markup
+from whathappened.campaign import forms
+from whathappened.character.models import Character
+
+
+def test_add_character_form(app):
+    def blank():
+        return [Character(id=1, title="foo_character")]
+
+    with app.test_request_context("/"):
+        form = forms.AddCharacterForm()
+
+    form.character.query_factory = blank
+
+    assert isinstance(form, forms.AddCharacterForm)
+    assert form.hidden_tag() == Markup("")
+    assert form.character() == Markup(
+        '<select id="character" name="character"><option value="1">foo_character</option></select>'
+    )
+    assert form.submit() == Markup(
+        '<input id="submit" name="submit" type="submit" value="Add character">'
+    )
