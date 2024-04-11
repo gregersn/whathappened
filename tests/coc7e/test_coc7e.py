1d6f5ba0813ff9dd594430d2d33230f8719b6d29
Greger Stolt Nilsen <gregersn@gmail.com>
2024-04-11 09:46:54 +0200

    Upgrade pytest -> 8.1.1

--- tests/coc7e/test_coc7e.py
+++ tests/coc7e/test_coc7e.py
@@ -1,4 +1,5 @@
 """Test functions specific to Call of Cthulhu."""
+
 from pathlib import Path
 import json
 import pytest
@@ -28,22 +29,22 @@ def test_half():
     assert half(20) == 10
 
 
-@pytest.fixture(name='dholes_sheet')
+@pytest.fixture(name="dholes_sheet")
 def fixture_dholes_sheet() -> dict:
     """Load character sheet from JSON and convert to dict."""
     sheet = None
-    filename = BASEDIR / 'testchar_dholes.json'
-    with open(filename, 'r') as input_file:
+    filename = BASEDIR / "testchar_dholes.json"
+    with open(filename, "r") as input_file:
         sheet = json.load(input_file)
 
     return sheet
 
 
-@pytest.fixture(name='test_sheet')
+@pytest.fixture(name="test_sheet")
 def fixture_test_sheet() -> dict:
     """Load character sheet from JSON and convert to dict."""
     sheet = None
-    with open(BASEDIR / 'testchar.json', 'r') as input_file:
+    with open(BASEDIR / "testchar.json", "r") as input_file:
         sheet = json.load(input_file)
 
     return sheet
@@ -57,7 +58,7 @@ def fixture_test_character() -> Character:
     return c
 
 
-def test_validate(test_sheet: dict):
+def test_validate(test_sheet: dict, app):
     nc = new_character("Test Character", "Classic (1920's)")
     errors = validate(nc, "coc7e")
     assert len(errors) == 0
@@ -72,22 +73,22 @@ def test_convert_from_dholes(dholes_sheet: dict):
     assert len(errors) == 0, error_messages
 
 
-def test_personalia_and_attributes(newly_created_character: Character):
+def test_personalia_and_attributes(newly_created_character: Character, app):
     assert newly_created_character.name == "Unknown"
     assert newly_created_character.age == "18"
     assert newly_created_character.description == "Unknown"
-    assert newly_created_character.system == 'coc7e'
+    assert newly_created_character.system == "coc7e"
     assert newly_created_character.game[0] == "Call of Cthulhu TM"
     assert newly_created_character.game[1] == "Classic (1920's)"
 
 
-def test_skills(newly_created_character: Character):
-    skill = newly_created_character.skill('Spot Hidden')
+def test_skills(newly_created_character: Character, app):
+    skill = newly_created_character.skill("Spot Hidden")
     assert skill is not None
-    assert skill['value'] == 25
+    assert skill["value"] == 25
 
 
-def test_skill(newly_created_character: Character):
+def test_skill(newly_created_character: Character, app):
     skill_name = "test skill"
     skill = newly_created_character.skill(skill_name)
     assert skill is None
@@ -95,19 +96,21 @@ def test_skill(newly_created_character: Character):
     newly_created_character.mechanics.add_skill(skillname=skill_name, value=13)
     skill = newly_created_character.skill(skill_name)
     assert skill is not None
-    assert skill['value'] == 13
+    assert skill["value"] == 13
 
     with pytest.raises(ValueError):
         newly_created_character.mechanics.add_skill(skillname=skill_name, value=154)
 
-    newly_created_character.set_attribute({'category': 'skill', 'field': skill_name, 'value': 21})
+    newly_created_character.set_attribute(
+        {"category": "skill", "field": skill_name, "value": 21}
+    )
 
     skill = newly_created_character.skill(skill_name)
     assert skill is not None
-    assert skill['value'] == 21
+    assert skill["value"] == 21
 
 
-def test_subskill(newly_created_character: Character):
+def test_subskill(newly_created_character: Character, app):
     skill_name = "Science"
     subskill_name = "Biology"
 
@@ -116,30 +119,37 @@ def test_subskill(newly_created_character: Character):
     subskill = newly_created_character.skill(skill_name, subskill_name)
 
     assert subskill is not None
-    assert subskill['value'] == skill['value']
+    assert subskill["value"] == skill["value"]
 
     with pytest.raises(ValueError):
         newly_created_character.mechanics.add_subskill(subskill_name, skill_name)
 
-    newly_created_character.set_attribute({'category': 'skill', 'field': skill_name, 'subfield': subskill_name, 'value': 21})
+    newly_created_character.set_attribute(
+        {
+            "category": "skill",
+            "field": skill_name,
+            "subfield": subskill_name,
+            "value": 21,
+        }
+    )
 
     subskill = newly_created_character.skill(skill_name, subskill_name)
     assert subskill is not None
-    assert subskill['value'] == 21
+    assert subskill["value"] == 21
 
 
-def test_validate_migration_up(test_sheet: dict):
+def test_validate_migration_up(test_sheet: dict, app):
     errors = validate(migrate(test_sheet, "0.0.4", migrations=migrations), "coc7e")
     assert len(errors) == 0, errors
 
 
-def test_validate_migration_latest(test_sheet: dict):
+def test_validate_migration_latest(test_sheet: dict, app):
     errors = validate(migrate(test_sheet, latest, migrations=migrations), "coc7e")
 
     assert len(errors) == 0, errors
 
 
-def test_validate_migration_up_and_down(test_sheet: dict):
+def test_validate_migration_up_and_down(test_sheet: dict, app):
     migrated = migrate(test_sheet.copy(), "0.0.4", migrations=migrations)
 
     back_down = migrate(migrated, "0.0.1", migrations=migrations)
