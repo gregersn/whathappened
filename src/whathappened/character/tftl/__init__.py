5bf85c5f69663ae15837667048065af694b00821
Greger Stolt Nilsen <gregersn@gmail.com>
2024-12-23 20:40:07 +0100

    Fix missing new_character import.

--- src/whathappened/character/tftl/__init__.py
+++ src/whathappened/character/tftl/__init__.py
@@ -1,2 +1,6 @@
+from whathappened.sheets.mechanics.core import new_character
+
 CREATE_TEMPLATE = "character/tftl/create.html.jinja"
 CHARACTER_SHEET_TEMPLATE = "character/tftl/sheet.html.jinja"
+
+__all__ = ["new_character"]
