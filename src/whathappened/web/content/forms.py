0c74045ed6b7959adb2f1c5e465726f4637f2064
Greger Stolt Nilsen <gregersn@gmail.com>
2025-01-18 14:01:39 +0100

    Linting of web.auth and web.content.

--- src/whathappened/web/content/forms.py
+++ src/whathappened/web/content/forms.py
@@ -1,13 +1,17 @@
+"""Content forms."""
+
 from wtforms.fields.simple import HiddenField, SubmitField
-from flask_wtf.form import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, UUID, Optional
+from flask_wtf.form import FlaskForm
 
 from whathappened.web.forms.fields.alchemy import QuerySelectField
 from whathappened.web.auth.utils import current_user
 
 
 class NewFolderForm(FlaskForm):
+    """New folder."""
+
     title = StringField("Title", validators=[DataRequired()])
     owner_id = HiddenField(validators=[DataRequired()])
     # parent_id = QuerySelectField('Parent')
@@ -18,10 +22,13 @@ class NewFolderForm(FlaskForm):
 
 
 def available_folders():
-    return current_user.profile.folders  # pyright: ignore[reportGeneralTypeIssues]
+    """Get available folders"""
+    return current_user.profile.folders
 
 
 class ChooseFolderForm(FlaskForm):
+    """Choose a folder."""
+
     folder_id = QuerySelectField(
         query_factory=available_folders, get_label=lambda x: x.title, allow_blank=True
     )
