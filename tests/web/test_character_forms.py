83f769397d39cf47e39e5e43e6fd194fe6b039ce
Greger Stolt Nilsen <gregersn@gmail.com>
2025-01-21 19:41:22 +0100

    More lint work

--- tests/web/test_character_forms.py
+++ tests/web/test_character_forms.py
@@ -4,3 +4,7 @@ from whathappened.web.character.forms import ImportForm
 def test_edit_json(app):
     form = ImportForm()
     assert not form.validate()
+
+    form.body.data = "{}"
+    form.title.data = "test import form"
+    assert form.validate(), form.errors
